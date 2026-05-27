0c46ca1fd4834ee50ad1d42fcb4efb3876d319e5
raiomacuin16 <raimaco123@gmail.com>
2026-05-26 22:17:06 -0500

    Add files via upload

---
+++ Comedor/src/main/java/Comedor/Enums/TipoComida.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Comedor.Enums;
+
+/**
+ *
+ * @author F20LAB10E03
+ */
+public enum TipoComida {
+    DESAYUNO,
+    ALMUERZO,
+    CENA
+}
---
+++ Comedor/src/main/java/Comedor/Usuarios/PersonalAdministrativo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Comedor.Usuarios;
+
+import java.util.Date;
+/**
+ *
+ * @author F20LAB10E03
+ */
+public class PersonalAdministrativo extends Usuario {
+    private String area;
+    private String cargo;
+    
+    public Date obtenerHorarioEntregaTicket(String tipoComida) {
+        
+    }
+    
+    public String obtenerTipoUsuario() {
+        
+    }
+}
